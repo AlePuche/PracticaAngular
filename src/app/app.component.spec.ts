537c71d38700a378e242e20dceb0128201054f9c
Alejandro <alepucheperez@gmail.com>
2024-09-29 23:43:35 +0200

    ngiF y ngFor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'SegundaPrueba'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SegundaPrueba');
+    expect(app.nombre).toEqual('Alejandro');
   });
 
   it('should render title', () => {
