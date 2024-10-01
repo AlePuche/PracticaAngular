2e913a43c9fa75236d37ad1ec6e8870550923f7f
Alejandro <alepucheperez@gmail.com>
2024-10-01 10:33:34 +0200

    fin practica

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -4,4 +4,15 @@ import { Component} from '@angular/core'
     selector: 'app-body',
     templateUrl: './body.component.html',
 })
-export class BodyComponent{}
+export class BodyComponent{
+
+    mostrar = true;
+
+    frase: any = {
+        mensaje: 'Las buenas decisiones vienen de la experiencia, pero la experiencia viene de malas decisiones',
+        autor: 'Mark Twain'
+    };
+
+    personajes: string[] = ['Bitcoin', 'Near', 'Render', 'RIO']
+
+}
